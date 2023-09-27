06eb952879a191aeb6efe035767245c49dd0b44a
NOTlAlGOODlIDEA <neithereauty@gmail.com>
2023-09-27 12:50:33 +0400

    App.java: removed Configuration annotation and ErrorPageRegistrar implementation; Added WebConfiguration

--- src/main/java/notagoodidea/App.java
+++ src/main/java/notagoodidea/App.java
@@ -2,21 +2,10 @@ package notagoodidea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.server.ErrorPage;
-import org.springframework.boot.web.server.ErrorPageRegistrar;
-import org.springframework.boot.web.server.ErrorPageRegistry;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 
-@Configuration
 @SpringBootApplication
-public class App implements ErrorPageRegistrar {
+public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-    @Override
-    public void registerErrorPages(ErrorPageRegistry registry) {
-        registry.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/not_found"));
     }
 }
---
+++ src/main/java/notagoodidea/WebConfiguration.java
@@ -0,0 +1,10 @@
+package notagoodidea;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+public class WebConfiguration {
+
+}
