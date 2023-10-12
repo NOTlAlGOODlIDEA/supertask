5996d5ba3e272449a1ef8534355f053af0abe843
NOTlAlGOODlIDEA <neithereauty@gmail.com>
2023-10-12 20:31:21 +0400

    Created config directory; Moved WebConfiguration to config package; Created Aspect configuration in config package

---
+++ src/main/java/notagoodidea/config/AspectJConfig.java
@@ -0,0 +1,10 @@
+package notagoodidea.config;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableAspectJAutoProxy
+public class AspectJConfig {
+
+}
--- src/main/java/notagoodidea/config/WebConfig.java
+++ src/main/java/notagoodidea/config/WebConfig.java
@@ -1,10 +1,10 @@
-package notagoodidea;
+package notagoodidea.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @Configuration
-public class WebConfiguration {
+public class WebConfig {
 
 }
