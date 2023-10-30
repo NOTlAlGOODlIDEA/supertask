1f3a8eac34b01fd19ad72358e86bf65cf7ee46d2
NOTlAlGOODlIDEA <neithereauty@gmail.com>
2023-10-30 21:06:34 +0400

    Moved LogInfoFromPostMethod from notagoodidea/ to notagoodidea/utils/annotations/

--- src/main/java/notagoodidea/utils/annotations/LogInfoFromPostMethod.java
+++ src/main/java/notagoodidea/utils/annotations/LogInfoFromPostMethod.java
@@ -1,11 +1,11 @@
-package notagoodidea.utils;
+package notagoodidea.utils.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(value = RetentionPolicy.RUNTIME)
 @Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.RUNTIME)
 public @interface LogInfoFromPostMethod {
 }
